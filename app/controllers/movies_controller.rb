6e143ea0c24424c277cc63f66c654d691159f325
vivianheleem <your.vivianhaleem@gmail.com>
2016-10-13 15:34:26 +0000

    fixed remote

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,81 @@
+# in app/controllers/movies_controller.rb
+
+# This file is app/controllers/movies_controller.rb
+
+class MoviesController < ApplicationController
+
+  def index
+
+    @movies = Movie.all
+
+  end
+
+def show
+
+  id = params[:id] # retrieve movie ID from URI route
+
+  @movie = Movie.find(id) # look up movie by unique ID
+
+  # will render app/views/movies/show.html.haml by default
+
+end
+
+private
+
+def movie_params
+params.require(:movie).permit(:title, :rating, :description, :release_date)
+end
+
+def new
+
+  @movie = Movie.new
+
+  # default: render 'new' template
+
+end 
+
+def create
+#@movie = Movie.create!(params[:movie]) #old way
+@movie = Movie.create!(movie_params) # new way
+flash[:notice] = "#{@movie.title} was successfully created."
+redirect_to movies_path
+end
+
+def update
+@movie = Movie.find params[:id]
+#@movie.update_attributes!(params[:movie]) # old way
+@movie.update_attributes!(movie_params) # new way  
+flash[:notice] = "#{@movie.title} was successfully updated."
+redirect_to movie_path(@movie)
+end
+
+def edit
+
+  @movie = Movie.find params[:id]
+
+end
+
+
+
+def update
+@movie = Movie.find params[:id]
+#@movie.update_attributes!(params[:movie]) # old way
+@movie.update_attributes!(movie_params) # new way  
+flash[:notice] = "#{@movie.title} was successfully updated."
+redirect_to movie_path(@movie)
+end
+
+def destroy
+
+    @movie = Movie.find(params[:id])
+
+    @movie.destroy
+
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+
+    redirect_to movies_path
+
+  end
+
+end
+
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Myrottenpotatoes::Application.routes.draw do
+
+  resources :movies
+
+  root :to => redirect('/movies')
+
+end
